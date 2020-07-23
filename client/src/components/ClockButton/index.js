b77f081076e6ed3a5722d9a36206eaf15e02e97e
Matthew Wester <MatthewWester@live.com>
2020-07-23 16:58:35 -0700

    Timer not in state now but as onclick

--- client/src/components/ClockButton/index.js
+++ client/src/components/ClockButton/index.js
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from "react";
+import ReactDom from 'react-dom';
+
 
 function ClockButton(InitTime) {
   const [time, setClock] = useState({seconds: InitTime.InitTime, level: 1});
 
-  setInterval(function(){
-    let newNum = time.seconds - 1;
-    setClock({seconds: newNum, level:1});
-  }, 1000);
+  function startTimer() {
+    let newNum = 1000
+    setInterval(function(){
+      newNum = newNum - 1;
+      console.log(newNum);
+  // ReactDOM.render("Hello", document.getElementById('timer'));
+    }, 1000);
+  }
     
   return (
     <div>
-    <button>
+    <button onClick={() => startTimer()}>
     <p>Level: {time.level}</p>
-    <p>{time.seconds}</p>
+    <scan>Click to start</scan>
       <p>Click to make progress!</p>
     </button>
     </div>
